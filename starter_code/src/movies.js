0f91d80a5b6c1154b2ddd3d98153348d7bd90adf
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-03-22 14:23:12 +0100

    Add iteration #1 solution

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,8 +1,62 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+// "2h 33min" -> 153
+// "2h"       -> 120
+// "33min"    -> 33
+
+// "2h"
+function convertHours (hourString) {
+  // ["2", ""]
+  var calculateHour = hourString.split("h");
+  return calculateHour[0] * 60;
+  // "2" * 60
+  // 120
+}
+
+// "33min"
+function convertMinutes (minuteString) {
+  // ["33", ""]
+  var calculateMinutes = minuteString.split("min");
+  return Number(calculateMinutes[0]);
+  // 33
+}
+
+function convertDuration (duration) {
+  var timePieces = duration.split(" ");
+  // ["2h", "33min"]
+  // ["2h"]
+  // ["33min"]
+
+  var minutes =
+    timePieces.reduce(function (sum, onePiece) {
+      if (onePiece.includes("h")) {
+        return sum + convertHours(onePiece);
+      }
+      return sum + convertMinutes(onePiece);
+    }, 0);
+
+  return minutes;
+}
+
+function turnHoursToMinutes (movies) {
+  var newCentArray =
+    movies.map(function (oneMovie) {
+      var newMovie = {};
+      newMovie.title = oneMovie.title;
+      newMovie.year = oneMovie.year;
+      newMovie.director = oneMovie.director;
+      newMovie.duration = convertDuration(oneMovie.duration);
+      newMovie.genre = oneMovie.genre;
+      newMovie.rate = oneMovie.rate;
+
+      return newMovie;
+    });
+
+  return newCentArray;
+}
 
 
-// Get the average of all rates with 2 decimals 
+// Get the average of all rates with 2 decimals
 
 
 // Get the average of Drama Movies
