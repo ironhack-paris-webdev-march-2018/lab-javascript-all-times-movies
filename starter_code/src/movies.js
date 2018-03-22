ca64aba92e8d8b4f527bfefa27ea7fd8f963560b
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-03-22 15:40:51 +0100

    Add solution

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -57,18 +57,121 @@ function turnHoursToMinutes (movies) {
 
 
 // Get the average of all rates with 2 decimals
+function ratesAverage (movies) {
+  var rateSum =
+    movies.reduce(function (sum, oneMovie) {
+      return sum + oneMovie.rate;
+    }, 0);
+
+  var result = rateSum / movies.length;
+  return Math.round(result * 100) / 100;
+}
 
 
 // Get the average of Drama Movies
+function dramaOnly (movies) {
+  var dramas =
+    movies.filter(function (oneMovie) {
+      return oneMovie.genre.includes("Drama");
+    });
+
+  return dramas;
+}
+function dramaMoviesRate (movies) {
+  var dramas = dramaOnly(movies);
+
+  if (dramas.length === 0) {
+    return;
+  }
+
+  return ratesAverage(dramas);
+}
 
 
 // Order by time duration, in growing order
+function orderByDuration (movies) {
+  movies.sort(function (movieA, movieB) {
+    var result = movieA.duration - movieB.duration;
+    if (result !== 0) {
+      return result;
+    }
+
+    if (movieA.title < movieB.title) {
+      return -1;
+    }
+
+    if (movieA.title > movieB.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return movies;
+}
 
 
 // How many movies did STEVEN SPIELBERG
+function howManyMovies (movies) {
+  if (movies.length === 0) {
+    return;
+  }
+
+  var spielbergFilms =
+    movies.filter(function (oneMovie) {
+      return oneMovie.director === "Steven Spielberg";
+    });
+
+  var dramas = dramaOnly(spielbergFilms);
+
+  return "Steven Spielberg directed " + dramas.length + " drama movies!";
+}
 
 
 // Order by title and print the first 20 titles
+function orderAlphabetically (movies) {
+  var sortedTitles =
+    movies
+      .map(function (oneMovie) {
+        return oneMovie.title;
+      })
+      .sort()
+      .slice(0, 20)
+    ;
+
+  return sortedTitles;
+}
 
 
 // Best yearly rate average
+function bestYearAvg (movies) {
+  if (movies.length === 0) {
+    return;
+  }
+
+  var movieYears = {};
+  movies.forEach(function (oneMovie) {
+    if (!movieYears[oneMovie.year]) {
+      movieYears[oneMovie.year] = [];
+    }
+
+    movieYears[oneMovie.year].push(oneMovie);
+  });
+
+  var winner =
+    Object.keys(movieYears)
+      .map(function (oneYear) {
+        return {
+          average: ratesAverage(movieYears[oneYear]),
+          year: oneYear
+        };
+      })
+      .reduce(function (currentBest, yearInfo) {
+        if (yearInfo.average > currentBest.average) {
+          return yearInfo;
+        }
+        return currentBest;
+      });
+
+  return "The best year was " + winner.year + " with an average rate of " + winner.average;
+}
